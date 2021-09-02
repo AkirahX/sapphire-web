a3d2e523a47ca0fd7ad857d8ac174d0581da6c89
AkirahX <akirah.ofc@gmail.com>
2021-09-02 18:42:47 -0300

    link

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,11 @@
+import Link from 'next/link'
 function Home(){
-    return <div>Home</div>
+    return <div>
+        <h1>Home</h1>
+
+        <Link href = '/sobre'>
+            <a>Pagina sobre</a>
+        </Link>
+    </div>
 }
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,11 @@
+import Link from 'next/link'
+function Sobre(){
+    return <div>
+        <h1>Home</h1>
+
+        <Link href = '/'>
+            <a>Pagina home</a>
+        </Link>
+    </div>
+}
+export default Sobre
